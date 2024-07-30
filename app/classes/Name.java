993f460771468d101d63df54f309e9b194dbcd37
Ishirokun <earljeroldcsanjose@gmail.com>
2024-07-31 02:08:22 +0800

    removed the non-existent suffix property reference

--- app/classes/Name.java
+++ app/classes/Name.java
@@ -4,14 +4,14 @@ package app.classes;
 public class Name{
     String firstName, lastName, middleName;
 
-    Name(String firstName, String lastName, String middleName){
+    public Name(String firstName, String lastName, String middleName){
         this.firstName = firstName;
         this.lastName = lastName;
         this.middleName = middleName;
     }
 
     public String getFullName(){
-        return this.firstName + " " + this.middleName + " " + this.lastName + ", " + this.suffix;
+        return this.firstName + " " + this.middleName + " " + this.lastName;
     }
 
     @Override
